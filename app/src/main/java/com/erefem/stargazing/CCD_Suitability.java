344fdd9c1549e7000f95d71fb846206ebd806ab7
yob601 <boystokist601@gmail.com>
2022-07-15 04:48:32 +0800

    listview onclick (tambahkan desain kalkulator di masing2 xml)

---
+++ app/src/main/java/com/erefem/stargazing/CCD_Suitability.java
@@ -0,0 +1,14 @@
+package com.erefem.stargazing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CCD_Suitability extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ccd_suitability);
+    }
+}
