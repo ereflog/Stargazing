7da92fdbf44dad17119f0ea6fc814d9dea03149c
yob601 <boystokist601@gmail.com>
2022-08-05 21:52:00 +0800

    input formula CCD Telescope combination

--- app/src/main/java/com/erefem/stargazing/CCDTelescopeCombinationCalc.java
+++ app/src/main/java/com/erefem/stargazing/CCDTelescopeCombinationCalc.java
@@ -3,8 +3,19 @@ package com.erefem.stargazing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CCDTelescopeCombinationCalc extends AppCompatActivity {
+
+    EditText
+            et_pixel_size, et_focal_lenght;
+    TextView
+            tv_resolution;
+
+    Button
+            btn_resolution;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +24,26 @@ public class CCDTelescopeCombinationCalc extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(R.string.ccd_telescope_combination_calc_title);
         }
+        et_pixel_size = findViewById(R.id.et_pixel_size);
+        et_focal_lenght = findViewById(R.id.et_focal_lenght);
+
+        tv_resolution = findViewById(R.id.tv_resolution);
+
+        btn_resolution = findViewById(R.id.btn_resolution);
+
+        btn_resolution.setOnClickListener(view -> {
+
+            if (SUtils.isEmpty(et_pixel_size)) {
+                SUtils.setToast(et_pixel_size,"is Empty!!!");
+            } else if (SUtils.isEmpty(et_focal_lenght)) {
+                SUtils.setToast(et_focal_lenght,"is Empty!!!");
+            } else {
+                float Pixel_size = Float.parseFloat(et_pixel_size.getText().toString());
+                float Focal_lenght = Integer.parseInt(et_focal_lenght.getText().toString());
+                float Resolution =
+                        (float) (Pixel_size / Focal_lenght * 206.265);
+                tv_resolution.setText(String.valueOf(Resolution));
+            }
+        });
     }
 }
