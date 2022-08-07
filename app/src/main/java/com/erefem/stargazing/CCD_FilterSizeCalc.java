41b11428ca97c044deadf35f963ab8ddb16c3e2e
Anton Purwanto <anton.takaful@gmail.com>
2022-08-07 11:29:11 +0700

    add lib function getFloatSpinnerValue, getFloat & fixing calculate CCD_FilterSizeCalc class

--- app/src/main/java/com/erefem/stargazing/CCD_FilterSizeCalc.java
+++ app/src/main/java/com/erefem/stargazing/CCD_FilterSizeCalc.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CCD_FilterSizeCalc extends AppCompatActivity {
 
     EditText
             et_fs_aperture, et_fs_fl, et_fs_diagonal, et_fs_distance;
     TextView tv_fs_min;
+
+    Spinner dd_barlow_reducer_ccd_dropdown;
 
     Button btn_fs_min;
 
@@ -27,6 +31,8 @@ public class CCD_FilterSizeCalc extends AppCompatActivity {
         et_fs_fl = findViewById(R.id.et_fs_fl);
         et_fs_diagonal = findViewById(R.id.et_fs_diagonal);
         et_fs_distance = findViewById(R.id.et_fs_distance);
+        dd_barlow_reducer_ccd_dropdown=findViewById(R.id.dd_barlow_reducer_ccd_dropdown);
+
 
         tv_fs_min = findViewById(R.id.tv_fs_min);
 
@@ -44,8 +50,9 @@ public class CCD_FilterSizeCalc extends AppCompatActivity {
                 SUtils.setToast(et_fs_distance,"is Empty!!!");
 
             } else {
+                float dd_barlow_reducer = SUtils.getFloatSpinnerValue(dd_barlow_reducer_ccd_dropdown);
                 float Aperture = SUtils.getInteger(et_fs_aperture);
-                float Focal_length = SUtils.getInteger(et_fs_fl);
+                float Focal_length = SUtils.getFloat(et_fs_fl) * dd_barlow_reducer;
                 float diagonal = Float.parseFloat(et_fs_diagonal.getText().toString());
                 float Distance = SUtils.getInteger(et_fs_distance);
 
--- app/src/main/java/com/erefem/stargazing/SUtils.java
+++ app/src/main/java/com/erefem/stargazing/SUtils.java
@@ -1,6 +1,7 @@
 package com.erefem.stargazing;
 
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +19,24 @@ public class SUtils {
     public static Double getDouble(EditText editTxt){
         return Double.parseDouble(editTxt.getText().toString());
     }
+
+    @NonNull
+    public static Float getFloat(EditText editTxt){
+        return Float.parseFloat(editTxt.getText().toString());
+    }
+
+    @NonNull
+    public static Float getFloatSpinnerValue(Spinner spinner){
+        float fl;
+        String tspinner = spinner.getSelectedItem().toString();
+        if(tspinner.equalsIgnoreCase("none") ) {
+            fl = 1.0f;
+        }else{
+           fl = Float.parseFloat(tspinner);
+        }
+        return fl;
+    }
+
     public static Boolean isEmpty(EditText editTxt){
         return editTxt.getText().toString().isEmpty();
     }
@@ -26,8 +45,5 @@ public class SUtils {
         editTxt.requestFocus();
         editTxt.setError(string);
     }
-
-
-
 
 }
