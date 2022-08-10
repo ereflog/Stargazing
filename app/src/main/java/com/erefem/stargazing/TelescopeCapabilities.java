51dcb0186b481776faf399b227af8c54979cab12
fortunofaqih <fortuntun@protonmail.com>
2022-08-11 00:28:46 +0700

    add layout telescope calculator, unit converter, redlight, obs logbook
    obs log bokk tiba tiba force close pak, mohon dikoreksi

--- app/src/main/java/com/erefem/stargazing/TelescopeCapabilities.java
+++ app/src/main/java/com/erefem/stargazing/TelescopeCapabilities.java
@@ -3,8 +3,18 @@ package com.erefem.stargazing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TelescopeCapabilities extends AppCompatActivity {
+
+    EditText
+            et_tele_aper, et_tele_ray,et_tele_limit,et_larger_light,et_smaller_light;
+    TextView
+            tv_max_resolution,tv_max_resolution_aper,tv_max_resolution_limiting,tv_max_resolution_light;
+    Button
+            btn_dawes,btn_rayleigh,btn_limiting,btn_light;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,23 @@ public class TelescopeCapabilities extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(R.string.telescope_capabilities_calc_title);
         }
+
+        et_tele_aper = findViewById(R.id.et_tele_aper);
+        et_tele_ray = findViewById(R.id.et_tele_ray);
+        et_tele_limit = findViewById(R.id.et_tele_limit);
+        et_larger_light = findViewById(R.id.et_larger_light);
+        et_smaller_light = findViewById(R.id.et_smaller_light);
+
+
+        tv_max_resolution = findViewById(R.id.tv_max_resolution);
+        tv_max_resolution_aper = findViewById(R.id.tv_max_resolution_aper);
+        tv_max_resolution_limiting = findViewById(R.id.tv_max_resolution_limiting);
+        tv_max_resolution_light = findViewById(R.id.tv_max_resolution_light);
+
+
+        btn_dawes = findViewById(R.id.btn_dawes);
+        btn_rayleigh = findViewById(R.id.btn_rayleigh);
+        btn_limiting = findViewById(R.id.btn_limiting);
+        btn_light = findViewById(R.id.btn_light);
     }
-}
+    }
