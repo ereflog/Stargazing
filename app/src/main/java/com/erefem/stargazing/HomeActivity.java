e73167e00d4f09eaabbdf326ea820fab4c5a2174
Anton Purwanto <anton.takaful@gmail.com>
2022-08-14 06:56:57 +0700

    Analytics Kit integrated: user attributes getAAID & popular reportPageEvent (custom event)

--- app/src/main/java/com/erefem/stargazing/HomeActivity.java
+++ app/src/main/java/com/erefem/stargazing/HomeActivity.java
@@ -9,9 +9,23 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.huawei.hms.analytics.HiAnalytics;
+import com.huawei.hms.analytics.HiAnalyticsInstance;
+import com.huawei.hms.analytics.HiAnalyticsTools;
+
+import static com.huawei.hms.analytics.type.HAEventType.*;
+import static com.huawei.hms.analytics.type.HAParamType.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class HomeActivity extends AppCompatActivity {
 
     ListView listView;
+
+    // TODO: Define a variable for the Analytics Kit instance.
+    HiAnalyticsInstance instance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,12 @@ public class HomeActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(R.string.app_name);
         }
+
+        // TODO: Initialize Analytics Kit.
+        // Enable Analytics Kit logging.
+        HiAnalyticsTools.enableLog();
+        // Generate an Analytics Kit instance.
+        instance = HiAnalytics.getInstance(this);
 
         listView = findViewById(R.id.listview);
 
@@ -59,72 +79,106 @@ public class HomeActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                instance.setUserProfile("viewOnClick_homePageResult",instance.getAAID().getResult()); // Obtains the execution result of a task if it is completed.
                 if (i == 0){
                     Intent intent = new Intent(view.getContext(),FocalLenghtRatioCalc.class);
+                    reportPageEvt(FocalLenghtRatioCalc.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 1){
                     Intent intent = new Intent(view.getContext(),MagnificationCalc.class);
+                    reportPageEvt(MagnificationCalc.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 2){
                     Intent intent = new Intent(view.getContext(),FieldOfViewCalc.class);
+                    reportPageEvt(FieldOfViewCalc.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 3){
                     Intent intent = new Intent(view.getContext(),TelescopeCapabilities.class);
+                    reportPageEvt(TelescopeCapabilities.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 4){
                     Intent intent = new Intent(view.getContext(),BinocularCalc.class);
+                    reportPageEvt(BinocularCalc.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 5){
                     Intent intent = new Intent(view.getContext(),CCD_Calc.class);
+                    reportPageEvt(CCD_Calc.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 6){
                     Intent intent = new Intent(view.getContext(),CCDTelescopeCombinationCalc.class);
+                    reportPageEvt(CCDTelescopeCombinationCalc.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 7){
                     Intent intent = new Intent(view.getContext(),CCD_FilterSizeCalc.class);
+                    reportPageEvt(CCD_FilterSizeCalc.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 8){
                     Intent intent = new Intent(view.getContext(),ImagingGuidingRatioCalc.class);
+                    reportPageEvt(ImagingGuidingRatioCalc.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 9){
                     Intent intent = new Intent(view.getContext(),UnitConverter.class);
+                    reportPageEvt(UnitConverter.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 10){
                     Intent intent = new Intent(view.getContext(),ObsLogBook.class);
+                    reportPageEvt(ObsLogBook.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 11){
                     Intent intent = new Intent(view.getContext(),WeatherForecast.class);
+                    reportPageEvt(WeatherForecast.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 12){
                     Intent intent = new Intent(view.getContext(),RedLightTool.class);
+                    reportPageEvt(RedLightTool.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 13){
                     Intent intent = new Intent(view.getContext(),About.class);
+                    reportPageEvt(About.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 14){
                     Intent intent = new Intent(view.getContext(),CreditAndDataSource.class);
+                    reportPageEvt(CreditAndDataSource.class.getSimpleName());
                     startActivity(intent);
                 }
                 if (i == 15){
                     Intent intent = new Intent(view.getContext(),PrivacyPolicy.class);
+                    reportPageEvt(PrivacyPolicy.class.getSimpleName());
                     startActivity(intent);
                 }
             }
         });
 
+    }
+
+    private void reportPageEvt(String page) {
+        // TODO: Report a psge event.
+        // Event name: Page
+        // Event parameters:
+        //  -- page: String
+        //  -- answerTime: String
+
+        // Initialize parameters.
+        Bundle bundle = new Bundle();
+        bundle.putString("page",page);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+        bundle.putString("answerTime",sdf.format(new Date()));
+
+        // Report a custom event.
+        instance.onEvent("Page", bundle);
     }
 }
