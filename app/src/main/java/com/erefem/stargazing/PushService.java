932e4f2bfb88d648d9981cdbe78347262e11008b
ereflog <mfr.skywatcher@gmail.com>
2022-08-23 01:19:16 +0700

    Add Push Kit

---
+++ app/src/main/java/com/erefem/stargazing/PushService.java
@@ -0,0 +1,50 @@
+package com.erefem.stargazing;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.huawei.hms.push.HmsMessageService;
+import com.huawei.hms.push.RemoteMessage;
+
+public class PushService extends HmsMessageService {
+    private static final String TAG = "PushMsgLog";
+    @Override
+    public void onNewToken(String token, Bundle bundle){
+        super.onNewToken(token);
+        Log.i(TAG, "receive token: " + token);
+        sendTokenToDisplay(token);
+    }
+
+    @Override
+    public void onTokenError(Exception e){
+        super.onTokenError(e);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){
+        super.onMessageReceived(remoteMessage);
+
+        if (remoteMessage.getData().length() > 0){
+            Log.i(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+        if (remoteMessage.getNotification() != null){
+            Log.i(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+
+    @Override
+    public void onMessageSent(String s){
+    }
+
+    @Override
+    public void onSendError(String s, Exception e){
+    }
+
+    private void sendTokenToDisplay(String token){
+        Intent intent = new Intent("com.erefem.stargazing.ON_NEW_TOKEN");
+        intent.putExtra("token", token);
+        sendBroadcast(intent);
+    }
+}
