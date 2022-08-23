e602a2635c2b424405339ee15ad0d4d8290e86d9
yob601 <boystokist601@gmail.com>
2022-08-23 23:23:18 +0800

    add PrivacyPolicy

--- app/src/main/java/com/erefem/stargazing/PrivacyPolicy.java
+++ app/src/main/java/com/erefem/stargazing/PrivacyPolicy.java
@@ -2,16 +2,32 @@ package com.erefem.stargazing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class PrivacyPolicy extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.privacy_policy);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(R.string.privacy_policy_title);
-        }
+
+        clickableHyperlink();
+    }
+    private void clickableHyperlink() {
+        TextView
+                linkTextView = findViewById(R.id.tv_privacy_police);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        linkTextView.setLinkTextColor(Color.BLUE);
     }
 }
