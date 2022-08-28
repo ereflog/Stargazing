bf589e79d514c49e7da9201ad59b2e806bdb451e
ereflog <mfr.skywatcher@gmail.com>
2022-08-29 01:57:51 +0700

    Fix focal length & focal ratio calc

--- app/src/main/java/com/erefem/stargazing/FocalLenghtRatioCalc.java
+++ app/src/main/java/com/erefem/stargazing/FocalLenghtRatioCalc.java
@@ -8,19 +8,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class FocalLenghtRatioCalc extends AppCompatActivity {
 
     EditText
-            etapertur,
-            etfocalratio,
-            etfocallenght,
-            etaperturratio;
+            etAperture1,
+            etFocalRatio,
+            etFocalLength,
+            etAperture2;
     TextView
-            tvfocallenght,
-            tvfocalrasio;
+            etFocalLengthRatioResult,
+            etFocalRatioResult;
     Button
-            btnfl,
-            btnfr;
+            btnFocalLength,
+            btnFocalRatio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,45 +33,43 @@ public class FocalLenghtRatioCalc extends AppCompatActivity {
             getSupportActionBar().setTitle(R.string.focal_calc_title);
         }
 
-        etapertur = findViewById(R.id.etapertur);
-        etfocalratio = findViewById(R.id.etfocalratio);
-        etfocallenght = findViewById(R.id.etfocallenght);
-        etaperturratio = findViewById(R.id.etaperturratio);
+        etAperture1 = findViewById(R.id.et_aperture1);
+        etFocalRatio = findViewById(R.id.et_focal_ratio);
+        etFocalLength = findViewById(R.id.et_focal_length);
+        etAperture2 = findViewById(R.id.et_aperture2);
 
-        tvfocallenght = findViewById(R.id.tvfocallenght);
-        tvfocalrasio = findViewById(R.id.tvfocalrasio);
+        etFocalLengthRatioResult = findViewById(R.id.et_focal_length_result);
+        etFocalRatioResult = findViewById(R.id.et_focal_ratio_result);
 
-        btnfl = findViewById(R.id.btnfl);
-        btnfr = findViewById(R.id.btnfr);
+        btnFocalLength = findViewById(R.id.btn_focal_length);
+        btnFocalRatio = findViewById(R.id.btn_focal_ratio);
 
-        btnfl.setOnClickListener(view -> {
+        DecimalFormat decimal = new DecimalFormat("#.##");
+        decimal.setRoundingMode(RoundingMode.HALF_EVEN);
 
-            if (SUtils.isEmpty(etapertur)) {
-                Toast.makeText(FocalLenghtRatioCalc.this, "Aperture is Empty!!!", Toast.LENGTH_SHORT).show();
-            } else if (SUtils.isEmpty(etfocalratio)) {
-                Toast.makeText(FocalLenghtRatioCalc.this, "Focal Ratio is Empty!!!", Toast.LENGTH_SHORT).show();
+        btnFocalLength.setOnClickListener(view -> {
+            if (SUtils.isEmpty(etAperture1)) {
+                SUtils.setToast(etAperture1, "This field cannot be blank!");
+            } if (SUtils.isEmpty(etFocalRatio)) {
+                SUtils.setToast(etFocalRatio, "This field cannot be blank!");
             } else {
-                //float Aperture = Integer.parseInt(etapertur.getText().toString());
-                float Aperture = SUtils.getInteger(etapertur);
-                float Focalratio = SUtils.getInteger(etfocalratio);
-                float Focallenght = Aperture * Focalratio;
-                tvfocallenght.setText(String.valueOf(Focallenght));
+                float aperture = SUtils.getInteger(etAperture1);
+                float focalRatio = SUtils.getInteger(etFocalRatio);
+                float focalLength = aperture * focalRatio;
+                etFocalLengthRatioResult.setText(decimal.format(focalLength));
             }
         });
 
-        btnfr.setOnClickListener(view -> {
-
-            if (SUtils.isEmpty(etfocallenght)) {
-                Toast.makeText(FocalLenghtRatioCalc.this, "Focal Lenght is Empty!!!", Toast.LENGTH_SHORT).show();
-            } else if (SUtils.isEmpty(etaperturratio)) {
-                Toast.makeText(FocalLenghtRatioCalc.this, "Aperture is Empty!!!", Toast.LENGTH_SHORT).show();
+        btnFocalRatio.setOnClickListener(view -> {
+            if (SUtils.isEmpty(etFocalLength)) {
+                SUtils.setToast(etFocalLength, "This field cannot be blank!");
+            } else if (SUtils.isEmpty(etAperture2)) {
+                SUtils.setToast(etAperture2, "This field cannot be blank!");
             } else {
-                //float Focallenght = Integer.parseInt(etfocallenght.getText().toString());
-                float Focallenght =SUtils.getInteger(etfocallenght);
-                //float Apertureratio = Integer.parseInt(etaperturratio.getText().toString());
-                float Apertureratio=SUtils.getInteger(etaperturratio);
-                float Focalratio = Focallenght / Apertureratio;
-                tvfocalrasio.setText(String.valueOf(Focalratio));
+                float focalLength = SUtils.getInteger(etFocalLength);
+                float aperture = SUtils.getInteger(etAperture2);
+                float focalRatio = focalLength / aperture;
+                etFocalRatioResult.setText(decimal.format(focalRatio));
             }
         });
     }
